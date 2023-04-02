07b8b381f3c97d01f8ee9ab63caa5a0b7d692990
Alex <trevan98@gmail.com>
2023-04-02 08:35:42 +0300

    serving images to frontend

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,23 @@ dotenv.config();
 
 const app = express();
 const { PORT } = process.env;
+const corsOptions = { origin: "http://localhost:5173", credentials: true };
 
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.get("/api/images", (req, res) => {
   const directoryPath = "./public";
   fs.readdir(directoryPath, (error, files) => {
-    error ? console.log("Unable to scan directory: " + error) : null;
+    error && console.log("Unable to scan directory: " + error);
     const images = files.filter((file) => file.endsWith(".jpg"));
     res.json(images);
-    console.log(images);
   });
 });
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
