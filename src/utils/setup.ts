9dd3f2b22719ba196f46e10af2189a00e8300926
Alex <trevan98@gmail.com>
2023-05-07 21:36:15 +0300

    test

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -15,6 +15,6 @@ const setupApp = (app: Express) => {
   app.use(cors(corsOptions));
   app.use(express.json({ limit: "50mb" }));
   app.use(cookieParser());
-  app.listen(port, () => console.log(`Example app listening on port ${port}`));
+  app.listen(8080, () => console.log(`Example app listening on port ${8080}`));
 };
 export { setupRoutes, setupApp };
