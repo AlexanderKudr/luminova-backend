12197ec4411f1addd3774db71a1de1fd0511ab58
Alex <trevan98@gmail.com>
2023-07-11 01:01:27 +0300

    test 2

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,13 @@ const setupApp = (app: Express) => {
   app.use(cookieParser());
   app.use(express.static("public"));
 
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
   swagger(app);
   app.listen(8080, () => console.log(`Example app listening on port ${8080}`));
 };
