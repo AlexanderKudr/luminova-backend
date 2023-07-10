24c7bf8020eba0e09ef66d62f2d2c033e5c750e2
Alex <trevan98@gmail.com>
2023-07-11 01:11:27 +0300

    test 3

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, PORT } = pro
 
 const config = {
   port: PORT,
-  corsOptions: { origin: "http://localhost:5173", credentials: true },
+  // corsOptions: { origin: "http://localhost:5173", credentials: true },
   cloudinary: {
     cloud_name: CLOUDINARY_NAME,
     api_key: CLOUDINARY_API_KEY,
