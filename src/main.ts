497537173a193cbfb915f71bdf764ac50c56180c
Alex <trevan98@gmail.com>
2023-07-11 01:06:34 +0300

    test 3

--- src/main.ts
+++ src/main.ts
@@ -15,12 +15,12 @@ const setupApp = (app: Express) => {
   app.use(express.static("public"));
 
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http:/127.0.0.1:5173/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
-  
+
   swagger(app);
   app.listen(8080, () => console.log(`Example app listening on port ${8080}`));
 };
