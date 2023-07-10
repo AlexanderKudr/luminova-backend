edf3bf8f1610ead1581c6fae405d37e6a6d9b0d4
Alex <trevan98@gmail.com>
2023-07-11 01:22:51 +0300

    test 5

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,8 @@ const setupApp = (app: Express) => {
 
   app.use((req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', 'http:/127.0.0.1:5173/');
-    res.header({'Access-Control-Allow-Origin': 'http:/127.0.0.1:5173/'});
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
