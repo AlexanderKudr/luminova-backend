7885949060275289143214b58f3f5633997b8bf2
Alex <trevan98@gmail.com>
2023-07-11 18:33:25 +0300

    preview up

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
-// const baseUrl = "http://v2004009.hosted-by-vdsina.ru";
-const baseUrl = "http://localhost:8080";
+const baseUrl = "http://v2004009.hosted-by-vdsina.ru";
+// const baseUrl = "http://localhost:8080";
 
 export { baseUrl };
