1064f304d558af51d28efc491a15cd85a52ea187
Alex <trevan98@gmail.com>
2023-07-23 20:37:15 +0300

    ups

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,6 @@ const app = express();
 
 const setupApp = (app: Express) => {
   app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-  // app.use((req, res, next) => {
-  //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  //   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //   res.setHeader("Access-Control-Allow-Credentials", "true");
-  //   next();
-  // });
   app.use(express.json({ limit: "50mb" }));
   app.use(cookieParser());
   app.use(express.static("public"));
