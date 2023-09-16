b315121c63f35b0c7eb598ad95acbed12f08ff8c
Alex <trevan98@gmail.com>
2023-09-16 23:53:06 +0300

    fix6

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ const app = express();
 
 const setupApp = (app: Express) => {
   app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-  
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
   app.use(express.json({ limit: "50mb" }));
   app.use(cookieParser());
   app.use(express.static("public"));
