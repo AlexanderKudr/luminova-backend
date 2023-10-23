6b6a3d44afdaf76d14f0bdc5e65e81dc9d1f7ad3
Alex <trevan98@gmail.com>
2023-10-23 06:00:37 +0300

    preparing for deploy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { swagger } from "./docs";
 const app = express();
 
 const setupApp = (app: Express) => {
-  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+  app.use(cors({ origin: "luminova.netlify.app", credentials: true }));
   app.use(express.json({ limit: "50mb" }));
   app.use(cookieParser());
   app.use(express.static("public"));
